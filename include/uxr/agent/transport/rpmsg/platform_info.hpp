974cd707dec95d498ceff6c09269db0e11e086e7
Vincent Conus <vincent.conus@pm.me>
2025-02-23 09:01:02 +0000

    add all the extra source for userspace rpmsg

---
+++ include/uxr/agent/transport/rpmsg/platform_info.hpp
@@ -0,0 +1,10 @@
+#ifndef PLATFORM_INFO_H
+#define PLATFORM_INFO_H
+
+#include <openamp/remoteproc.h>
+#include <openamp/virtio.h>
+#include <openamp/rpmsg.h>
+
+#include "platform_info_common.hpp"
+
+#endif /* PLATFORM_INFO_H */
---
+++ src/cpp/transport/rpmsg/rsc_table.cpp
@@ -0,0 +1,47 @@
+#include <openamp/open_amp.h>
+#include "rsc_table.h"
+
+#define RPMSG_VDEV_DFEATURES        (1 << VIRTIO_RPMSG_F_NS)
+
+/* VirtIO rpmsg device id */
+#define VIRTIO_ID_RPMSG_             7
+
+#define NUM_VRINGS                  0x02
+#define VRING_ALIGN                 0x1000
+#define RING_TX                     0x00004000
+#define RING_RX                     0x00008000
+#define VRING_SIZE                  256
+
+#define NUM_TABLE_ENTRIES           1
+
+struct remote_resource_table resources = {
+	/* Version */
+	1,
+
+	/* NUmber of table entries */
+	NUM_TABLE_ENTRIES,
+	/* reserved fields */
+	{0, 0,},
+
+	/* Offsets of rsc entries */
+	{
+	 offsetof(struct remote_resource_table, rpmsg_vdev),
+	},
+
+	/* Virtio device entry */
+	{
+	 RSC_VDEV, VIRTIO_ID_RPMSG_, 0, RPMSG_VDEV_DFEATURES, 0, 0, 0,
+	 NUM_VRINGS, {0, 0},
+	 },
+
+	/* Vring rsc entry - part of vdev rsc entry */
+	{RING_TX, VRING_ALIGN, VRING_SIZE, 1, 0},
+	{RING_RX, VRING_ALIGN, VRING_SIZE, 2, 0},
+};
+
+void *get_resource_table (int rsc_id, int *len)
+{
+	(void) rsc_id;
+	*len = sizeof(resources);
+	return &resources;
+}
