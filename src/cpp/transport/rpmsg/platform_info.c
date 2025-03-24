b421d61932b662137c68e6af087baa730405de98
Vincent Conus <vincent.conus@pm.me>
2025-03-24 06:46:35 +0000

    consistent formatting for the platform info c file

--- src/cpp/transport/rpmsg/platform_info.c
+++ src/cpp/transport/rpmsg/platform_info.c
@@ -40,72 +40,79 @@ static struct rpmsg_virtio_shm_pool shpool;
 static struct remoteproc *
 platform_create_proc(int proc_index, int rsc_index)
 {
-	void *rsc_table;
-	int rsc_size;
-	int ret;
-	metal_phys_addr_t pa;
+  void *rsc_table;
+  int rsc_size;
+  int ret;
+  metal_phys_addr_t pa;
 
-	(void)proc_index;
-	(void)rsc_index;
-	rsc_size = RSC_MEM_SIZE;
+  (void)proc_index;
+  (void)rsc_index;
+  rsc_size = RSC_MEM_SIZE;
 
-	/* Initialize remoteproc instance */
-	if (!remoteproc_init(&rproc_inst, &zynqmp_linux_r5_proc_ops,
-			     &rproc_priv))
-		return NULL;
-	printf("Successfully initialized remoteproc\r\n");
+  /* Initialize remoteproc instance */
+  if (!remoteproc_init(&rproc_inst, &zynqmp_linux_r5_proc_ops,
+		       &rproc_priv))
+    return NULL;
+  printf("Successfully initialized remoteproc\r\n");
 
-	/* Mmap resource table */
-	pa = RSC_MEM_PA;
-	printf("Calling mmap resource table.\r\n");
-	rsc_table = remoteproc_mmap(&rproc_inst, &pa, NULL, rsc_size,
-				    0, NULL);
-	if (!rsc_table) {
-		fprintf(stderr, "ERROR: Failed to mmap resource table.\r\n");
-		return NULL;
-	}
-	printf("Successfully mmap resource table.\r\n");
-	/* parse resource table to remoteproc */
-	ret = remoteproc_set_rsc_table(&rproc_inst, rsc_table, rsc_size);
-	if (ret) {
-		printf("Failed to initialize remoteproc\r\n");
-		remoteproc_remove(&rproc_inst);
-		return NULL;
-	}
-	printf("Successfully set resource table to remoteproc.\r\n");
+  /* Mmap resource table */
+  pa = RSC_MEM_PA;
+  printf("Calling mmap resource table.\r\n");
+  rsc_table = remoteproc_mmap(&rproc_inst, &pa, NULL, rsc_size,
+			      0, NULL);
+  if (!rsc_table)
+    {
+      fprintf(stderr, "ERROR: Failed to mmap resource table.\r\n");
+      return NULL;
+    }
+  printf("Successfully mmap resource table.\r\n");
+  /* parse resource table to remoteproc */
+  ret = remoteproc_set_rsc_table(&rproc_inst, rsc_table, rsc_size);
+  if (ret)
+    {
+      printf("Failed to initialize remoteproc\r\n");
+      remoteproc_remove(&rproc_inst);
+      return NULL;
+    }
+  printf("Successfully set resource table to remoteproc.\r\n");
 
-	return &rproc_inst;
+  return &rproc_inst;
 }
 
-int platform_init(int argc, char *argv[], void **platform)
+int
+platform_init(int argc, char *argv[], void **platform)
 {
-	unsigned long proc_id = 0;
-	unsigned long rsc_id = 0;
-	struct remoteproc *rproc;
+  unsigned long proc_id = 0;
+  unsigned long rsc_id = 0;
+  struct remoteproc *rproc;
 
-	if (!platform) {
-		fprintf(stderr, "Failed to initialize platform, NULL pointer"
-			"to store platform data.\r\n");
-		return -EINVAL;
-	}
-	/* Initialize HW system components */
-	init_system();
+  if (!platform)
+    {
+      fprintf(stderr, "Failed to initialize platform, NULL pointer"
+	      "to store platform data.\r\n");
+      return -EINVAL;
+    }
+  /* Initialize HW system components */
+  init_system();
 
-	if (argc >= 2) {
-		proc_id = strtoul(argv[1], NULL, 0);
-	}
+  if (argc >= 2)
+    {
+      proc_id = strtoul(argv[1], NULL, 0);
+    }
 
-	if (argc >= 3) {
-		rsc_id = strtoul(argv[2], NULL, 0);
-	}
+  if (argc >= 3)
+    {
+      rsc_id = strtoul(argv[2], NULL, 0);
+    }
 
-	rproc = platform_create_proc(proc_id, rsc_id);
-	if (!rproc) {
-		fprintf(stderr, "Failed to create remoteproc device.\r\n");
-		return -EINVAL;
-	}
-	*platform = rproc;
-	return 0;
+  rproc = platform_create_proc(proc_id, rsc_id);
+  if (!rproc)
+    {
+      fprintf(stderr, "Failed to create remoteproc device.\r\n");
+      return -EINVAL;
+    }
+  *platform = rproc;
+  return 0;
 }
 
 struct  rpmsg_device *
@@ -114,51 +121,54 @@ platform_create_rpmsg_vdev(void *platform, unsigned int vdev_index,
 			   void (*rst_cb)(struct virtio_device *vdev),
 			   rpmsg_ns_bind_cb ns_bind_cb)
 {
-	struct remoteproc *rproc = platform;
-	struct rpmsg_virtio_device *rpmsg_vdev;
-	struct virtio_device *vdev;
-	void *shbuf;
-	struct metal_io_region *shbuf_io;
-	int ret;
+  struct remoteproc *rproc = platform;
+  struct rpmsg_virtio_device *rpmsg_vdev;
+  struct virtio_device *vdev;
+  void *shbuf;
+  struct metal_io_region *shbuf_io;
+  int ret;
 
-	rpmsg_vdev = metal_allocate_memory(sizeof(*rpmsg_vdev));
-	if (!rpmsg_vdev)
-		return NULL;
-	shbuf_io = remoteproc_get_io_with_pa(rproc, SHARED_BUF_PA);
-	if (!shbuf_io)
-		goto err1;
-	shbuf = metal_io_phys_to_virt(shbuf_io,
-				      SHARED_BUF_PA);
+  rpmsg_vdev = metal_allocate_memory(sizeof(*rpmsg_vdev));
+  if (!rpmsg_vdev)
+    return NULL;
+  shbuf_io = remoteproc_get_io_with_pa(rproc, SHARED_BUF_PA);
+  if (!shbuf_io)
+    goto err1;
+  shbuf = metal_io_phys_to_virt(shbuf_io,
+				SHARED_BUF_PA);
 
-	printf("Creating virtio...\r\n");
-	/* TODO: can we have a wrapper for the following two functions? */
-	vdev = remoteproc_create_virtio(rproc, vdev_index, role, rst_cb);
-	if (!vdev) {
-		printf("failed remoteproc_create_virtio\r\n");
-		goto err1;
-	}
-	printf("Successfully created virtio device.\r\n");
+  printf("Creating virtio...\r\n");
+  /* TODO: can we have a wrapper for the following two functions? */
+  vdev = remoteproc_create_virtio(rproc, vdev_index, role, rst_cb);
+  if (!vdev)
+    {
+      printf("failed remoteproc_create_virtio\r\n");
+      goto err1;
+    }
+  printf("Successfully created virtio device.\r\n");
 
-	/* Only RPMsg virtio driver needs to initialize the shared buffers pool */
-	rpmsg_virtio_init_shm_pool(&shpool, shbuf, SHARED_BUF_SIZE);
+  /* Only RPMsg virtio driver needs to initialize the shared buffers pool */
+  rpmsg_virtio_init_shm_pool(&shpool, shbuf, SHARED_BUF_SIZE);
 
-	printf("initializing rpmsg vdev\r\n");
-	/* RPMsg virtio device can set shared buffers pool argument to NULL */
-	ret = rpmsg_init_vdev(rpmsg_vdev, vdev, ns_bind_cb,
-			      shbuf_io, &shpool);
-	if (ret) {
-		printf("failed rpmsg_init_vdev\r\n");
-		goto err2;
-	}
-	return rpmsg_virtio_get_rpmsg_device(rpmsg_vdev);
-err2:
-	remoteproc_remove_virtio(rproc, vdev);
-err1:
-	metal_free_memory(rpmsg_vdev);
-	return NULL;
+  printf("initializing rpmsg vdev\r\n");
+  /* RPMsg virtio device can set shared buffers pool argument to NULL */
+  ret = rpmsg_init_vdev(rpmsg_vdev, vdev, ns_bind_cb,
+			shbuf_io, &shpool);
+  if (ret)
+    {
+      printf("failed rpmsg_init_vdev\r\n");
+      goto err2;
+    }
+  return rpmsg_virtio_get_rpmsg_device(rpmsg_vdev);
+ err2:
+  remoteproc_remove_virtio(rproc, vdev);
+ err1:
+  metal_free_memory(rpmsg_vdev);
+  return NULL;
 }
 
-int platform_poll(void *priv)
+int
+platform_poll(void *priv)
 {
   struct remoteproc *rproc = priv;
   struct remoteproc_priv *prproc;
@@ -169,37 +179,40 @@ int platform_poll(void *priv)
   while(1)
     {
       flags = metal_irq_save_disable();
-      if (!(atomic_flag_test_and_set(&prproc->ipi_nokick))) {
-	metal_irq_restore_enable(flags);
-	ret = remoteproc_get_notification(rproc,
-					  RSC_NOTIFY_ID_ANY);
-	if (ret)
-	  return ret;
-	break;
-      }
+      if (!(atomic_flag_test_and_set(&prproc->ipi_nokick)))
+	{
+	  metal_irq_restore_enable(flags);
+	  ret = remoteproc_get_notification(rproc,
+					    RSC_NOTIFY_ID_ANY);
+	  if (ret)
+	    return ret;
+	  break;
+	}
       _rproc_wait();
       metal_irq_restore_enable(flags);
     }
   return 0;
 }
 
-void platform_release_rpmsg_vdev(struct rpmsg_device *rpdev, void *platform)
+void
+platform_release_rpmsg_vdev(struct rpmsg_device *rpdev, void *platform)
 {
-	struct rpmsg_virtio_device *rpvdev;
-	struct remoteproc *rproc;
+  struct rpmsg_virtio_device *rpvdev;
+  struct remoteproc *rproc;
 
-	rpvdev = metal_container_of(rpdev, struct rpmsg_virtio_device, rdev);
-	rproc = platform;
+  rpvdev = metal_container_of(rpdev, struct rpmsg_virtio_device, rdev);
+  rproc = platform;
 
-	rpmsg_deinit_vdev(rpvdev);
-	remoteproc_remove_virtio(rproc, rpvdev->vdev);
+  rpmsg_deinit_vdev(rpvdev);
+  remoteproc_remove_virtio(rproc, rpvdev->vdev);
 }
 
-void platform_cleanup(void *platform)
+void
+platform_cleanup(void *platform)
 {
-	struct remoteproc *rproc = platform;
+  struct remoteproc *rproc = platform;
 
-	if (rproc)
-		remoteproc_remove(rproc);
-	cleanup_system();
+  if (rproc)
+    remoteproc_remove(rproc);
+  cleanup_system();
 }
