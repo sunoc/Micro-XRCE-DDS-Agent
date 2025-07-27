424b70eb3dd7da37901edf013598bce9193de014
Vincent Conus <sunoc@localhost.localdomain>
2025-07-27 11:38:15 +0900

    add the helper source for urp into src

---
+++ src/cpp/transport/rpmsg/helper.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdio.h>
+#include <metal/sys.h>
+
+int
+init_system()
+{
+  struct metal_init_params metal_param = METAL_INIT_DEFAULTS;
+
+  metal_init(&metal_param);
+
+  return 0;
+}
+
+void
+cleanup_system()
+{
+  metal_finish();
+}
